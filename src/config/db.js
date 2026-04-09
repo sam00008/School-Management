93818faa7d3831f46af41f3ed7329bd8b2a34414
Samee <sameealam50@gmail.com>
2026-04-09 23:21:57 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,7 @@ let db;
 
 const connetDB = async () => {
   try {
-     db = await mysql.createConnection({
-       uri: process.env.DATABASE_URL,
-       ssl: { rejectUnauthorized: true },
-   });
+  db = await mysql.createConnection(process.env.DATBASE_URL);
 
     console.log("MySQL Connected Successfully");
     //console.log( await db.execute("show databases"));
